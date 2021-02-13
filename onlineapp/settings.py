002bfb612b1b68b398d7f05f2f2aee242b84287c
onenfelix <onenfelix90@gmail.com>
2021-02-13 12:00:58 +0300

    added more settings for django

--- onlineapp/settings.py
+++ onlineapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,6 +127,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = "/static/"
+# STATIC_ROOT = "/static/"
 
 STATIC_URL = '/static/'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
